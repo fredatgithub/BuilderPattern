ebf54436757c31d90d405ae6f85e8e8f6184f024
Fred <domaine75@gmail.com>
2021-03-23 17:47:24 +0100

    Initial commit

---
+++ BuilderDemo/SuperSandwich.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BuilderDemo
+{
+  class SuperSandwichBuilder : SandwichBuilder
+  {
+    public override void PrepareBread()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void ApplyMeatAndCheese()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void ApplyVegetables()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void AddCondiments()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
